4b2b7d5ab4066a569e2e30bab822198b1324a2cd
samuelwstout <samuelwstout@gmail.com>
2022-11-22 15:59:59 -0600

    remove /refresh

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,18 @@
 import bodyParser from "body-parser";
 import express from "express";
-import pg from "pg";
 import SpotifyWebApi from 'spotify-web-api-node';
 import cors from 'cors';
 require('dotenv').config();
-
-// Connect to the database using the DATABASE_URL environment
-//   variable injected by Railway
-const pool = new pg.Pool();
+const cookieParser = require('cookie-parser');
 
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.use(cookieParser());
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
 app.use(bodyParser.text({ type: "text/html" }));
-
-app.get("/", async (req, res) => {
-  res.send('Classify backend');
-});
-
-app.post('/refresh', (req, res) => {
-  const refreshToken = req.body.refreshToken;
-  const spotifyApi = new SpotifyWebApi({
-    redirectUri: process.env.REDIRECT_URI_LOCAL,
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    refreshToken,
-  })
-
-  spotifyApi.refreshAccessToken()
-  .then(data => {
-          res.json({
-              accessToken: data.body.access_token,
-              expiresIn: data.body.expires_in,
-          })
-      }).catch(() => {
-          res.sendStatus(400)
-      })
-  })
 
   app.post('/login', (req, res) => {
     res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
