2f0ed14b024746a0e8693a8e09b88c1e47dab4db
samuelwstout <samuelwstout@gmail.com>
2022-11-25 00:06:41 -0600

    return to previous reqs/setup

--- src/index.ts
+++ src/index.ts
@@ -3,26 +3,44 @@ import express from "express";
 import SpotifyWebApi from 'spotify-web-api-node';
 import cors from 'cors';
 require('dotenv').config();
-const cookieParser = require('cookie-parser');
 
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(cookieParser());
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
-app.use(bodyParser.text({ type: "text/html" }));
 
+app.get("/", async (req, res) => {
+  res.send('Classify backend');
+});
+
+app.post('/refresh', (req, res) => {
+    const refreshToken = req.body.refreshToken
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: process.env.REDIRECT_URI,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken,
+    })
+    spotifyApi.refreshAccessToken()
+    .then(data => {
+            res.json({
+                accessToken: data.body.access_token,
+                expiresIn: data.body.expires_in,
+            })
+        }).catch(() => {
+            res.sendStatus(400)
+        })
+    })
+  
   app.post('/login', (req, res) => {
     res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
     const code = req.body.code
     const spotifyApi = new SpotifyWebApi({
-        redirectUri: process.env.REDIRECT_URI_LOCAL,
+        redirectUri: process.env.REDIRECT_URI,
         clientId: process.env.CLIENT_ID,
         clientSecret: process.env.CLIENT_SECRET,
     })
-
     spotifyApi.authorizationCodeGrant(code).then(data => {
         res.json({
             accessToken: data.body.access_token,
@@ -34,6 +52,5 @@ app.use(bodyParser.text({ type: "text/html" }));
         res.sendStatus(error)
     })
 })
-
 
 app.listen(port);
